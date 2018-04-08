2e4a91ff196e9d2b8b41047bb782e4ce16f242a2
13686487169 <1123804727@qq.com>
2018-04-08 22:23:31 +0800

    多线程之银行转账

---
+++ src/cn/sp/chapter14/unsynch/Bank.java
@@ -0,0 +1,62 @@
+package cn.sp.chapter14.unsynch;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by 2YSP on 2018/4/8.
+ */
+public class Bank {
+
+    private final double[] accounts;
+
+    //使用锁来同步
+//    private Lock bankLock = new ReentrantLock();
+
+    /**
+     * 创建每个账户
+     * @param n
+     * @param initialBalance
+     */
+    public Bank(int n,double initialBalance){
+        accounts = new double[n];
+        for(int i=0;i<accounts.length;i++){
+            accounts[i] = initialBalance;
+        }
+    }
+
+    /**
+     * transfer money from one account to another
+     * @param from
+     * @param to
+     * @param amount
+     */
+    public void transfer(int from,int to,double amount){
+//        bankLock.lock();
+        try {
+            if (accounts[from] < amount){
+                return;
+            }
+            System.out.print(Thread.currentThread());
+            accounts[from] -= amount;
+            System.out.printf(" %10.2f from %d to %d",amount,from,to);
+            accounts[to] += amount;
+            System.out.printf(" Total Balance:%10.2f%n",getTotalBalance());
+        }finally {
+//            bankLock.unlock();
+        }
+
+    }
+
+    private double getTotalBalance() {
+        double sum = 0;
+        for(double a :accounts){
+            sum += a;
+        }
+        return sum;
+    }
+
+    public int size(){
+        return accounts.length;
+    }
+}
---
+++ src/cn/sp/chapter14/unsynch/UnsynchBankTest.java
@@ -0,0 +1,19 @@
+package cn.sp.chapter14.unsynch;
+
+/**
+ * 未同步情况模拟银行转账
+ * Created by 2YSP on 2018/4/8.
+ */
+public class UnsynchBankTest {
+
+    private static final int NACCOUNTS = 100;
+    private static final int INITIAL_BALANCE = 1000;
+    public static void main(String[] args) {
+        Bank bank = new Bank(NACCOUNTS,INITIAL_BALANCE);
+        for(int i=0;i<NACCOUNTS;i++){
+            Runnable r = new TransferRunnable(bank,i,INITIAL_BALANCE);
+            Thread t = new Thread(r);
+            t.start();
+        }
+    }
+}
